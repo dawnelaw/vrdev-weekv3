f45e07cfd1271c8455a408abd637fb3ccaad129e
Dawn Law <dawnelaw@gmail.com>
2021-10-02 15:29:04 -0600

    added sound

---
+++ week1v3/Assets/CollideAlley.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideAlley : MonoBehaviour
+{
+    Vector3 originalPos;
+
+    public AudioSource laneRoll;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+
+        laneRoll = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // Detect collision with floor
+    void OnCollisionEnter(Collision hit)
+    {
+
+        if (hit.gameObject.tag == "lane")
+        {
+            laneRoll.Play();
+        }
+    }
+}
---
+++ week1v3/Assets/CollideBall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideBall : MonoBehaviour
+{ 
+
+    public AudioSource pinStrike;
+  //  public AudioSource alleyRoll;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        pinStrike = GetComponent<AudioSource>();
+   //     alleyRoll = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // Detect collision with floor
+    void OnCollisionEnter(Collision hit)
+    {
+
+        if (hit.gameObject.tag == "pin")
+        {
+            pinStrike.Play();
+        }
+    }
+}
